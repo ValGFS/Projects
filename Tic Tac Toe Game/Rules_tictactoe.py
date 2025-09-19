a1fc38f4791059c8f79e59872238fd40acb3b57d
ValGFS <valentin.r.gfs@gmail.com>
2025-09-19 16:42:42 +0200

    First iteration: Put everithing inside a class and the functions will be the methods and the board & player will be the atributes

--- Tic Tac Toe Game/Rules_tictactoe.py
+++ Tic Tac Toe Game/Rules_tictactoe.py
@@ -1,95 +1,105 @@
-def create_board ():
-    return [''] * 9
+#Clase del juego
+class TicTacToe ():
+    def __init__(self):
+        self.board = [''] * 9   #Crear el tablero
+        self.current_player = 'X'
 
-def show_board (board):
-    def c(i):
-        return board[i] if board[i] else ' '
+    #Mostrar el tablero
+    def show_board (self):
+        def c(i):
+            return self.board[i] if self.board[i] else ' '
 
-    print('\n     Tic Tac Toe\n')
-    row = '    {:^3}|{:^3}|{:^3}   ({} | {} | {})'
-    sep = '    ---+---+---'
-    print (row.format(c(0), c(1), c(2), 1, 2, 3))
-    print (sep)
-    print (row.format(c(3), c(4), c(5), 4, 5, 6))
-    print(sep)
-    print (row.format(c(6), c(7), c(8), 7, 8, 9))
+        print('\n     Tic Tac Toe\n')
+        row = '    {:^3}|{:^3}|{:^3}   ({} | {} | {})'
+        sep = '    ---+---+---'
+        print (row.format(c(0), c(1), c(2), 1, 2, 3))
+        print (sep)
+        print (row.format(c(3), c(4), c(5), 4, 5, 6))
+        print(sep)
+        print (row.format(c(6), c(7), c(8), 7, 8, 9))
 
+    #Validación de movimiento
+    def valid_move (self, position):
+        index = position - 1
+        return 1 <= position <= 9 and self.board[index] == ''
 
-def valid_move (board, position):
-    index = position - 1
-    return 1 <= position <= 9 and board[index] == ''
+    #Validar el movimiento del jugador
+    def player_move (self, position):
+        index = position - 1
+        self.board[index] = self.current_player
 
-def player_move (board, position, player):
-    index = position - 1
-    board[index] = player
+    #Función para definir el ganador
+    def winner (self):
+        winning_lines = [
+            (0, 1, 2), (3, 4, 5), (6, 7, 8), #Combinaciones de lineas
+            (0, 3, 6), (1, 4, 7), (2, 5, 8), #Combinación de columnas
+            (0, 4, 8), (2, 4 ,6)             #Combinación en diagonal
+        ]
+        return any (self.board[a] == self.board[b] == self.board[c] == self.current_player for a, b, c in winning_lines)
 
-def winner (board, player):
-    winning_lines = [
-        (0, 1, 2), (3, 4, 5), (6, 7, 8),
-        (0, 3, 6), (1, 4, 7), (2, 5, 8),
-        (2, 5, 8), (0, 4, 8), (2, 4 ,6)
-    ]
-    return any (board[a] == board[b] == board[c] == player for a, b, c in winning_lines)
+    #Definicón de un empate
+    def draw (self):
+        return '' not in self.board
 
-def draw (board):
-    return '' not in board
+    #Cambio de jugador
+    def switch_player (self):
+        self.current_player = 'O' if self.current_player == 'X' else 'X'
 
-def switch_player (player):
-    return 'O' if player == 'X' else 'X'
+    #Función principal para correr el juego
+    def play (self):
+        self.board = [''] * 9
+        self.current_player ='X'
+        self.show_board()
 
-def play ():
-    board = create_board()
-    current_player ='X'
-    show_board(board)
+        while True:
+            try:
+                player_input = input (f'\nJugador {self.current_player}, elige una posicion del 1 al 9:').strip()
+                position = int(player_input)
+            except ValueError:
+                print (f'Posicion no valida, escribe un numero del 1  al 9')
+                continue
 
-    while True:
-        try:
-            player_input = input (f'\nJugador {current_player}, elige una posicion del 1 al 9:').strip()
-            position = int(player_input)
-        except ValueError:
-            print (f'Posicion no valida, escribe un numero del 1  al 9')
-            continue
+            if not self.valid_move(position):
+                print ('\nMovimiento invalido. Vuelve a intentar')
+                continue
 
-        if not valid_move(board, position):
-            print ('Movimiento invalido. Vuelve a intentar')
-            continue
+            self.player_move (position,)
+            self.show_board ()
 
-        player_move (board, position, current_player)
-        show_board(board)
+            if self.winner ():
+                print (f'\nFelicidades jugador {self.current_player} has ganado!')
+                break
 
-        if winner(board, current_player):
-            print (f'\n Felicidades jugador {current_player} has ganado!')
-            break
+            if self.draw ():
+                print (f'\nEmpate.\nNo hay mas movimientos. Juego finalizado.')
+                break
 
-        if draw (board):
-            print (f'\n No hay mas movimientos. Juego finalizado en empate.')
-            break
-
-        current_player = switch_player (current_player)
+            self.switch_player()
 
 def instructions ():
     print('Bievenido a Tic Tac Toe.\n')
     print('Las reglas son las siguientes:\n')
     print('1. Hay dos jugadores en cada partida. Uno utiliza la "X" y el otro usa la "O" como representacion para cada uno.')
     print('2. Selecciona un numero del 1 al 9, esto corresponde a la ubicacion donde se realizara tu jugada.')
-    print('3. Si la posicion ya esta ocupada, no puede ser seleccionado por el otro jugador.')
+    print('3. Si la posicion ya esta ocupada, no es posible volver a seleccionarlo.')
     print('4. Para poder ganar se debe realizar una linea vertical, horizontal o diagonal con un solo simbolo, ya sea con la "X" o la "O".')
     print('5. Si por alguna razon, se agotan los movimientos, el juego acabar en un empate.')
 
 def game_menu():
     while True:
-        print ('\n---Tic Tac Toe---\nSelecciona una opcion\n')
+        print ('\n---Tic Tac Toe---\nSeleccione una opcion\n')
         print ('1. Nueva Partida')
         print ('2. Instrucciones')
         print ('3. Salir')
 
         option = input ('\nSeleccione una opcion: ')
         if option == '1':
-            play()
+            game = TicTacToe()
+            game.play()
         elif option == '2':
             instructions()
         elif option == '3':
-            print ('Saliendo del juego. ¡Hasta luego!')
+            print ('\nSaliendo del juego. ¡Hasta luego!')
             break
         else:
             print ('\nOpcion no valida, Intenta nuevamente')
